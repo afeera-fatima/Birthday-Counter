17fb1e2395b8372c46b266c2e3a172aa1fda79fc
Afeera Fatima <120155223+afeera-fatima@users.noreply.github.com>
2026-03-10 03:52:14 +0500

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Feb 14, 2024',
-  name: "Zaineb"
+  birthdate: 'Mar 20, 2026',
+  name: "Ibtesam"
 };
 
 function hideEverything() {
